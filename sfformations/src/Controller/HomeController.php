ee5874fa2ff7a65653893a73d8ab8f0ff6339d13
Elmira PETIT <elledevelope@gmail.com>
2024-01-22 16:12:09 +0100

    Added `pagination` for homepage in `HomeController.php` and `templates\index.html.twig`

--- sfformations/src/Controller/HomeController.php
+++ sfformations/src/Controller/HomeController.php
@@ -3,20 +3,31 @@
 namespace App\Controller;
 
 use App\Repository\FormationsRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(FormationsRepository $formationRepository): Response
+    public function index(FormationsRepository $formationRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $formations = $formationRepository->findBy([], ['duree' => 'ASC']);
+        $formations = $paginator->paginate(
+            $formationRepository->findBy([], ['id' => 'DESC']),
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
 
         return $this->render('home/index.html.twig', [
             'formations' => $formations,
         ]);
+
+        // $formations = $formationRepository->findBy([], ['duree' => 'ASC']);
+        // return $this->render('home/index.html.twig', [
+        //     'formations' => $formations,
+        // ]);
 
     }
 }
